69a7e96bf0d63994430d70a9845ec8a37d3ee25e
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2021-05-05 05:21:10 +0200

    info++

--- src/info.rs
+++ src/info.rs
@@ -13,9 +13,9 @@
 // You should have received a copy of the GNU General Public License
 // along with rust-u4pak.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::pak::{Pak, COMPR_NONE, COMPR_ZLIB, COMPR_BIAS_MEMORY, COMPR_BIAS_SPEED};
+use crate::{pak::{Pak, COMPR_NONE, COMPR_ZLIB, COMPR_BIAS_MEMORY, COMPR_BIAS_SPEED}, util::print_table};
 use crate::result::Result;
-use crate::util::{format_size, print_headless_table, Align};
+use crate::util::{format_size, Align};
 
 pub fn info(pak: &Pak, human_readable: bool) -> Result<()> {
     let fmt_size = if human_readable {
@@ -32,38 +32,71 @@ pub fn info(pak: &Pak, human_readable: bool) -> Result<()> {
     let mut bias_memory_count = 0usize;
     let mut other_count       = 0usize;
     let mut encrypted_count   = 0usize;
-    // TODO: min/max (uncompressed/compression block) size
+    let mut sum_uncompr_size     = 0;
+    let mut sum_zlib_size        = 0;
+    let mut sum_bias_speed_size  = 0;
+    let mut sum_bias_memory_size = 0;
+    let mut sum_unknown_size     = 0;
+    let mut sum_encrypted_size   = 0;
+
+    let mut sum_uncompr_zlib_size        = 0;
+    let mut sum_uncompr_bias_speed_size  = 0;
+    let mut sum_uncompr_bias_memory_size = 0;
+    let mut sum_uncompr_unknown_size     = 0;
+    let mut sum_uncompr_encrypted_size   = 0;
 
     for record in pak.records() {
         sum_size += record.size();
         sum_uncompressed_size += record.uncompressed_size();
         if record.encrypted() {
             encrypted_count += 1;
+            sum_encrypted_size += record.size();
+            sum_uncompr_encrypted_size += record.uncompressed_size();
         }
         match record.compression_method() {
-            self::COMPR_NONE        => uncompr_count     += 1,
-            self::COMPR_ZLIB        => zlib_count        += 1,
-            self::COMPR_BIAS_SPEED  => bias_speed_count  += 1,
-            self::COMPR_BIAS_MEMORY => bias_memory_count += 1,
-            _                       => other_count       += 1,
+            self::COMPR_NONE => {
+                uncompr_count += 1;
+                sum_uncompr_size += record.size();
+            }
+            self::COMPR_ZLIB => {
+                zlib_count += 1;
+                sum_zlib_size += record.size();
+                sum_uncompr_zlib_size += record.uncompressed_size();
+            }
+            self::COMPR_BIAS_SPEED => {
+                bias_speed_count += 1;
+                sum_bias_speed_size += record.size();
+                sum_uncompr_bias_speed_size += record.uncompressed_size();
+            }
+            self::COMPR_BIAS_MEMORY => {
+                bias_memory_count += 1;
+                sum_bias_memory_size += record.size();
+                sum_uncompr_bias_memory_size += record.uncompressed_size();
+            }
+            _ => {
+                other_count += 1;
+                sum_unknown_size += record.size();
+                sum_uncompr_unknown_size += record.uncompressed_size();
+            }
         }
     }
 
-    print_headless_table(
+    println!("Pak Version: {}", pak.version());
+    println!("Mount Point: {}", pak.mount_point().unwrap_or(""));
+    println!();
+
+    print_table(
+        &["", "Count", "Size", "Uncompr."],
+        &[Align::Left, Align::Right, Align::Right, Align::Right],
         &[
-            vec!["Pak Version:",               &format!("{}", pak.version())],
-            vec!["Mount Point:",               pak.mount_point().unwrap_or("")],
-            vec!["Sum Compr. Size:",           &fmt_size(sum_size)],
-            vec!["Sum Uncompr. Size:",         &fmt_size(sum_uncompressed_size)],
-            vec!["File Count:",                &format!("{}", pak.records().len())],
-            vec!["Uncompr. Count:",            &format!("{}", uncompr_count)],
-            vec!["ZLIB Compr. Count:",         &format!("{}", zlib_count)],
-            vec!["Bias Speed Compr. Count:",   &format!("{}", bias_speed_count)],
-            vec!["Bias Memory Compr. Count:",  &format!("{}", bias_memory_count)],
-            vec!["Unknown Compression Count:", &format!("{}", other_count)],
-            vec!["Encrypted Count:",           &format!("{}", encrypted_count)],
+            vec!["Files:",              &format!("{}", pak.records().len()), &fmt_size(sum_size),             &fmt_size(sum_uncompressed_size)],
+            vec!["Uncompr.:",           &format!("{}", uncompr_count),       &fmt_size(sum_uncompr_size),     ""],
+            vec!["ZLIB Compr.:",        &format!("{}", zlib_count),          &fmt_size(sum_zlib_size),        &fmt_size(sum_uncompr_zlib_size)],
+            vec!["Bias Speed Compr.:",  &format!("{}", bias_speed_count),    &fmt_size(sum_bias_speed_size),  &fmt_size(sum_uncompr_bias_speed_size)],
+            vec!["Bias Memory Compr.:", &format!("{}", bias_memory_count),   &fmt_size(sum_bias_memory_size), &fmt_size(sum_uncompr_bias_memory_size)],
+            vec!["Unknown Compr.:",     &format!("{}", other_count),         &fmt_size(sum_unknown_size),     &fmt_size(sum_uncompr_unknown_size)],
+            vec!["Encrypted:",          &format!("{}", encrypted_count),     &fmt_size(sum_encrypted_size),   &fmt_size(sum_uncompr_encrypted_size)],
         ],
-        &[Align::Left, Align::Right]
     );
 
     Ok(())
