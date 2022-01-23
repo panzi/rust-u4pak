c0653b18cfeb025c1ed6e22ca8fd25e3b2cc84b1
Lasse Sprengel <1288524+Xenira@users.noreply.github.com>
2022-01-23 03:45:29 +0100

    Added v11 unpack tests

---
+++ tests/util/mod.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::num::NonZeroUsize;
+use std::path::Path;
+
+use u4pak::index::Encoding;
+use u4pak::pak::Options;
+use u4pak::unpack::UnpackOptions;
+use u4pak::util::{make_pak_path, sha1_digest};
+use u4pak::walkdir::{walkdir, WalkDir};
+use u4pak::{Error, Pak, Result, Variant};
+
+pub fn unpack(path: &str, outdir: &str, encryption: Option<String>) -> Result<()> {
+    let encryption_key = if let Some(key) = encryption {
+        Some(
+            base64::decode(
+                key.parse::<String>()
+                    .expect("Failed to read encryption key."),
+            )
+            .expect("Failed to parse encryption key."),
+        )
+    } else {
+        None
+    };
+
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(error) => return Err(Error::io_with_path(error, path)),
+    };
+    let mut reader = BufReader::new(&mut file);
+
+    let pak = Pak::from_reader(
+        &mut reader,
+        Options {
+            variant: Variant::default(),
+            ignore_magic: false,
+            encoding: Encoding::default(),
+            force_version: None,
+            encryption_key: encryption_key.clone(),
+        },
+    )?;
+
+    drop(reader);
+
+    u4pak::unpack::unpack(
+        &pak,
+        &mut file,
+        outdir,
+        UnpackOptions {
+            dirname_from_compression: false,
+            verbose: false,
+            null_separated: false,
+            paths: None,
+            thread_count: NonZeroUsize::new(num_cpus::get())
+                .unwrap_or(NonZeroUsize::new(1).unwrap()),
+            encryption_key,
+        },
+    )
+}
+
+pub fn validate(source_dir: &str, out_dir: &str) -> Result<()> {
+    let source_path = Path::new(source_dir);
+    let out_path = Path::new(out_dir);
+
+    let iter = match walkdir(source_dir) {
+        Ok(iter) => iter,
+        Err(err) => return Err(Error::io_with_path(err, source_dir)),
+    };
+
+    for entry in iter {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(error) => return Err(Error::io_with_path(error, source_dir)),
+        };
+        let file_path = entry.path();
+        let file = match file_path.strip_prefix(source_dir) {
+            Ok(file) => file,
+            Err(err) => return Err(Error::new(format!("Failed to strip prefix from {:?}", file_path).to_string()))
+        };
+
+        let out_path_buff = out_path.join(file);
+        let out_path = out_path_buff.as_path();
+        
+        if out_path.exists() {
+            let source_digest = sha1_digest(File::open(&file_path)?)?;
+            let out_digest = sha1_digest(File::open(out_path)?)?;
+            if source_digest != out_digest {
+                return Err(Error::new(format!("Source digest {:?} does not match out digest {:?} for file {:?}", source_digest, out_digest, file).to_string()));
+            }
+        } else {
+            return Err(Error::new(format!("File {:?} does not exist in output", file).to_string()));
+        }
+    }
+
+    Ok(())
+}
